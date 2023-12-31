5114d50ba18adf48665892a50c38a117ebbfb104
Ignat <ignatmalouhov@gmail.com>
2023-12-31 20:15:56 +0300

    Create project structure

---
+++ kafka-broker/src/test/java/demo/ignat/broker/KafkaBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ignat.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ kafka-consumer/src/test/java/demo/ignat/consumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ignat.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
